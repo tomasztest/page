9feb419f6071b4ba5cae84bdba689e1cf8a60351
tomasztest <tomaszkotzur@gmail.com>
2019-08-06 23:16:55 +0200

    Updates

---
+++ precache-manifest.2d3fe6644a133c72ec5d8dd80ed84c90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e20a99805c988a30c5baf1a9b76e20f7",
+    "url": "/page/index.html"
+  },
+  {
+    "revision": "159fbfc3d4edbe67b4a5",
+    "url": "/page/static/css/main.e0856415.chunk.css"
+  },
+  {
+    "revision": "a00e481d27cd986dc3d6",
+    "url": "/page/static/js/2.9b33cc78.chunk.js"
+  },
+  {
+    "revision": "159fbfc3d4edbe67b4a5",
+    "url": "/page/static/js/main.94114c27.chunk.js"
+  },
+  {
+    "revision": "52f0d7f9f0fc1aa12f89",
+    "url": "/page/static/js/runtime~main.493d39d8.js"
+  },
+  {
+    "revision": "0e9f2e8be835af09e8e83947dae40eb8",
+    "url": "/page/static/media/1.0e9f2e8b.jpg"
+  },
+  {
+    "revision": "4e26123821ce435c92bf5d6f31de32ac",
+    "url": "/page/static/media/2.4e261238.jpg"
+  },
+  {
+    "revision": "dfc3bc9ee0bf3857d73bb5cd8efa48d2",
+    "url": "/page/static/media/3.dfc3bc9e.jpg"
+  }
+]);
